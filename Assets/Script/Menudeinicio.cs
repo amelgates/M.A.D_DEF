c6dc0395c27d2864934471e16efccabff75b77e4
amelgates <mirrai2888@gmail.com>
2022-12-01 14:19:54 -0400

    creditos

--- Assets/Script/Menudeinicio.cs
+++ Assets/Script/Menudeinicio.cs
@@ -18,4 +18,8 @@ public class Menudeinicio : MonoBehaviour
     {
         SceneManager.LoadScene("Creditos");
     }
+    public void BackGame()
+    {
+        SceneManager.LoadScene("Inicio");
+    }
 }
